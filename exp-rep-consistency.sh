843baa858fbd88174a83a0396c3f2785f96edc60
Julio Cesar Mendoza Bobadilla <jmendozais@gmail.com>
2020-09-28 10:44:43 -0300

    Remove exp. scripts copies

--- exp-rep-consistency.sh
+++
@@ -1,6 +0,0 @@
-# Contribution 1. Representation consistency:
-# variations: 
-# - min reprojection
-# - soft min reprojection
-
-CUDA_VISIBLE_DEVICES=1 python -u $(pwd)/train.py --log ${DATADIR}/base-depth-lk-lr1e-5-imagenetnorm-regnet-repcons --weight-ds 1e-2 --weight-ofs 0 --weight-ec 0 -b 4 --epoch 10 --learn-intrinsics -l 1e-5 --rep-cons
--- exp-robust-losses.sh
+++
@@ -1,4 +0,0 @@
-# Contribution 2: Comprehensive evaluation of robust losses on the target problem
-# - Evaluation of robust fixed estimators
-# - Evaluation of adaptive tukey and huber estimators
-
