7296b554d8faf57ca51aace70bbdac2039ea0202
Julio Cesar Mendoza Bobadilla <jmendozais@gmail.com>
2022-04-24 22:13:15 -0300

    Remove unused file

--- dependencies.sh
+++
@@ -1,9 +0,0 @@
-apt-get update 
-apt-get install libturbojpeg
-apt-get install zlip-dev
-apt-get install libjpeg-dev
-apt-get install libsm6
-apt-get install libxrender1
-
-#pip uninstall pillow; CC="cc -mavx2" pip install -U --force-reinstall pillow-simd #TODO test SSE4
-
